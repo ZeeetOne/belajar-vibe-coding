238610720f332820a370c3baacf1e43a13ed127d
ZeeetOne <sulthondaffakautsar@gmail.com>
2026-04-21 10:35:28 +0700

    feat: implement user registration with password hashing and routing refactor

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,11 @@
 import { Elysia, t } from 'elysia';
 import { db } from './db';
-import { users, selectUserSchema, insertUserSchema } from './db/schema';
+import { usersRoute } from './routes/users-route';
 
 const app = new Elysia()
   .decorate('db', db)
   .get('/', () => ({ message: 'Welcome to ElysiaJS + Drizzle + MySQL!' }))
-  .group('/users', (app) =>
-    app
-      .get('/', async ({ db }) => {
-        return await db.select().from(users);
-      })
-      .post('/', async ({ db, body }) => {
-        const result = await db.insert(users).values(body);
-        return { success: true, id: result[0].insertId };
-      }, {
-        body: insertUserSchema
-      })
-  )
+  .use(usersRoute)
   .listen(3000);
 
 console.log(
